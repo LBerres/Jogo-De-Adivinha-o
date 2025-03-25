566eee7032d2ef0bcdaedb6fc1ab6739a12d4364
LBerres <Leandro.motorhead@gmail.com>
2025-03-24 22:57:19 -0300

    Implementada a Verção 3 - Verificação de Acertos,  Erros e Loop de Gameplay

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -1,22 +1,55 @@
-﻿namespace JogoDeAdivinhacao.ConsoleApp
+﻿using System.ComponentModel.Design;
+
+namespace JogoDeAdivinhacao.ConsoleApp
 {
     internal class Program
     {
-        // Versão 1: Estrutura Básica e Entrada do Usuário
+        // Versão 3: Verificar se o jogador acertou
         static void Main(string[] args)
         {
-            Console.WriteLine("-------------------------------------------------------------");
-            Console.WriteLine("Jogo de Adivinhação");
-            Console.WriteLine("-------------------------------------------------------------");
+            while (true)
+            {
+                Console.WriteLine("-------------------------------------------------------------");
+                Console.WriteLine("Jogo de Adivinhação");
+                Console.WriteLine("-------------------------------------------------------------");
 
-            // Lógica do Jogo
-            Console.Write("Digite Para Chutar: ");
-            int numerodigitado = Convert.ToInt32(Console.ReadLine());
+                // Lógica do Jogo
+                Random geradorDeNumeros = new Random();
 
-            Console.WriteLine("Você Digitou o Número: " + numerodigitado);
+                int numerosecreto = geradorDeNumeros.Next(1, 21);
 
+                Console.Write("Digite Um Número (de 1 à 20) Para Chutar: ");
+                int numerodigitado = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine();
+                if (numerodigitado == numerosecreto)
+                {
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.WriteLine("Parabéns, Você Acertou!");
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.ReadLine();
+                }
+                else if (numerodigitado > numerosecreto)
+                {
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.WriteLine("O Número Digitado Foi Maior que o Número Secreto!");
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.WriteLine("O Número Digitado Foi Menor que o Número Secreto!");
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.ReadLine();
+                }
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                            break;
+            }
+            
         }
     }
 }
