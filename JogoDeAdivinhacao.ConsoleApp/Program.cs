613e8abfa95d51438b9b5c0dcff4c956fead1898
LBerres <Leandro.motorhead@gmail.com>
2025-03-24 22:16:53 -0300

    Implementada a Verção 1 Com a Entrada e Saída de Dados

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+        // Versão 1: Estrutura Básica e Entrada do Usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            // Lógica do Jogo
+            Console.Write("Digite Para Chutar: ");
+            int numerodigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você Digitou o Número: " + numerodigitado);
+
+
+            Console.ReadLine();
         }
     }
 }
