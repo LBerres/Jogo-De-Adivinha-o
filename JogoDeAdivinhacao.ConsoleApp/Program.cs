1757e750dcf8116b1140b52bec5887ea41015569
LBerres <Leandro.motorhead@gmail.com>
2025-03-31 16:10:29 -0300

    Implementado Versão 4: Criar múltiplas tentativas

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -2,54 +2,97 @@
 
 namespace JogoDeAdivinhacao.ConsoleApp
 {
+    // Versão 4: Criar Múltiplas Tentativas
     internal class Program
     {
-        // Versão 3: Verificar se o jogador acertou
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.WriteLine("-------------------------------------------------------------");
+                Console.Clear();
+                Console.WriteLine("----------------------------------------");
                 Console.WriteLine("Jogo de Adivinhação");
-                Console.WriteLine("-------------------------------------------------------------");
+                Console.WriteLine("----------------------------------------");
 
-                // Lógica do Jogo
+                // Escolha de dificuldade
+                Console.WriteLine("Escolha Um Nível de Dificuldade:");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("1 - Fácil (10 Tentativas)");
+                Console.WriteLine("2 - Médio (5 Tentativas)");
+                Console.WriteLine("3 - Difícil (3 Tentativas)");
+                Console.WriteLine("----------------------------------------");
+
+                int totalDeTentativas = 0;
+
+                Console.Write("Digite Sua Escolha: ");
+                string entrada = Console.ReadLine();
+
+                if (entrada == "1")
+                    totalDeTentativas = 10;
+                else if (entrada == "2")
+                    totalDeTentativas = 5;
+                else if (entrada == "3")
+                    totalDeTentativas = 3;
+
+                // Geração de número secreto
                 Random geradorDeNumeros = new Random();
 
-                int numerosecreto = geradorDeNumeros.Next(1, 21);
+                int numeroSecreto = geradorDeNumeros.Next(1, 21);
 
-                Console.Write("Digite Um Número (de 1 à 20) Para Chutar: ");
-                int numerodigitado = Convert.ToInt32(Console.ReadLine());
-
-                if (numerodigitado == numerosecreto)
+                // Loop de tentativas do jogo
+                for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
                 {
-                    Console.WriteLine("-------------------------------------------------------------");
-                    Console.WriteLine("Parabéns, Você Acertou!");
-                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.Clear();
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine($"Tentativa {tentativa} de {totalDeTentativas}");
+                    Console.WriteLine("----------------------------------------");
+
+                    // Lógica do jogo
+                    Console.Write("Digite Um Número Entre 1 e 20: ");
+                    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                    if (numeroDigitado == numeroSecreto)
+                    {
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("Parabéns! Você Acertou!");
+                        Console.WriteLine("----------------------------------------");
+
+                        break;
+                    }
+
+                    if (tentativa == totalDeTentativas)
+                    {
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine($"Que Pena! Você Usou Todas as Tentativas. O Número Era {numeroSecreto}.");
+                        Console.WriteLine("----------------------------------------");
+
+                        break;
+                    }
+
+                    else if (numeroDigitado > numeroSecreto)
+                    {
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("O Número Digitado é Maior Que o Número Secreto.");
+                        Console.WriteLine("----------------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("O Número Digitado é Menor Que o Número Secreto.");
+                        Console.WriteLine("----------------------------------------");
+                    }
+
+                    Console.WriteLine("Aperte ENTER Para Continuar...");
                     Console.ReadLine();
                 }
-                else if (numerodigitado > numerosecreto)
-                {
-                    Console.WriteLine("-------------------------------------------------------------");
-                    Console.WriteLine("O Número Digitado Foi Maior que o Número Secreto!");
-                    Console.WriteLine("-------------------------------------------------------------");
-                    Console.ReadLine();
-                }
-                else
-                {
-                    Console.WriteLine("-------------------------------------------------------------");
-                    Console.WriteLine("O Número Digitado Foi Menor que o Número Secreto!");
-                    Console.WriteLine("-------------------------------------------------------------");
-                    Console.ReadLine();
-                }
 
-                Console.Write("Deseja continuar? (S/N): ");
+                Console.Write("Deseja Continuar? (S/N): ");
                 string opcaoContinuar = Console.ReadLine().ToUpper();
 
                 if (opcaoContinuar != "S")
-                            break;
+                    break;
             }
-            
+
         }
     }
 }
